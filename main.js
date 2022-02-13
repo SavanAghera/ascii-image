262786ce5b9314e9d1c860aae938679717eabc1f
SavanAghera <savanpatelasr@gmail.com>
2022-02-13 23:54:31 +0530

    optimized code and removed hsv color

--- main.js
+++ main.js
@@ -1,45 +1,26 @@
+const height = 60;
+const width = 100;
 const ascii = document.getElementById("ascii");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-const img = new Image(500, 500);
-img.addEventListener(
-  "load",
-  (e) => {
-    console.log("hiii");
-    ctx.drawImage(img, 0, 0, 200, 125);
-    let imageData = ctx.getImageData(0, 0, 200, 125).data;
-    console.log(imageData.filter((v, i) => i % 1 === 0));
-    // canvas.style.display = "none";
-    const data = [];
-    let value = "";
-    for (let i = 0; i < imageData.length; i += 4) {
-      let hsp = Math.sqrt(
-        0.299 * (imageData[i] * imageData[i]) +
-          0.587 * (imageData[i + 1] * imageData[i + 1]) +
-          0.114 * (imageData[i + 2] * imageData[i + 2])
-      );
-      if (hsp > 127.5) {
-        data.push(0);
-      } else {
-        data.push(1);
-      }
+const img = new Image(width, height);
+img.addEventListener("load", (e) => {
+  ctx.drawImage(img, 0, 0, width, height);
+  let imageData = ctx.getImageData(0, 0, width, height).data;
+  console.log(imageData.filter((v, i) => i % 1 === 0));
+  const data = [];
+  let value = "" , temp = "" , j = 0;;
+  for (let i = 0; i < imageData.length; i += 4) {
+    if (i % (width *4) === 0) {
+      value += temp + "<br/>";
+      temp = "";
+      j++;
     }
-    console.log(data);
-    let temp = "";
-    for (let i = 0, j = 0; i < data.length; i += 1) {
-      if (i % 200 === 0) {
-        value += temp + "<br/>";
-        temp = ''
-        j++;
-      }
-      if (j%1 ==0) {
-        if (data[i] === 1) {
-          temp += "a";
-        } else temp += "c";
-      }
-    }
-    ascii.innerHTML = value;
-  },
+    temp += `<span style='color:rgb(${imageData[i]}, ${imageData[i + 1]}, ${imageData[i + 2]})'>s</span>`;
+  }
+  console.log(data);
+  ascii.innerHTML = value;
+},
   false
 );
 console.log(img);
